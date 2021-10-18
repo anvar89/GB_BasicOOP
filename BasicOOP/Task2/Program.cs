01843a85443078948ab126d8284abdc0ee3b5b2b
khalitov <36979151+anvar89@users.noreply.github.com>
2021-10-18 17:45:54 +0300

    Задание 2 выполнено

---
+++ BasicOOP/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello World!";
+            string s2 = "321";
+            string s3 = "!";
+            string s4 = "";
+
+            Console.WriteLine($"{s1} -> {ReverseSymbols(s1)}");
+            Console.WriteLine($"{s2} -> {ReverseSymbols(s2)}");
+            Console.WriteLine($"{s3} -> {ReverseSymbols(s3)}");
+            Console.WriteLine($"{s4} -> {ReverseSymbols(s4)}");
+
+        }
+
+        public static string ReverseSymbols(string s)
+        {
+            var arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
