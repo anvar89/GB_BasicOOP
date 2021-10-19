afc55d19fe5150b594524887546e98d9a762c29b
khalitov <36979151+anvar89@users.noreply.github.com>
2021-10-19 16:18:16 +0300

    Задание 3. методы реализованы, нужно добавить демонстрационный пример

---
+++ BasicOOP/Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void GetMailList(string source, string target)
+        {
+            string sourceText = File.ReadAllText(source);
+            string targetText = string.Empty;
+            string[] mailList = sourceText.Split("&");
+
+            for (int i = 0; i < mailList.Length; i++)
+            {
+                SearchMail(ref mailList[i]);
+                targetText += mailList[i] + "\n";
+            }
+
+            File.WriteAllText(target + @"\mailList.txt", targetText);
+        }
+
+        public static void SearchMail(ref string s)
+        {
+            string[] arr = s.Split(" ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Contains("@"))
+                {
+                    s = arr[i];
+                    break;
+                }
+            }
+            s = string.Empty;
+        }
+    }
+}
