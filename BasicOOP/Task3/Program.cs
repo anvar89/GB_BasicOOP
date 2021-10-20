e6476f7d4f928ee272cc08c9d1bc8996ab951073
khalitov <36979151+anvar89@users.noreply.github.com>
2021-10-20 16:48:25 +0300

    Задание 3 выполнено

--- BasicOOP/Task3/Program.cs
+++ BasicOOP/Task3/Program.cs
@@ -7,22 +7,25 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetMailList("List.txt", "");
         }
 
         public static void GetMailList(string source, string target)
         {
             string sourceText = File.ReadAllText(source);
             string targetText = string.Empty;
-            string[] mailList = sourceText.Split("&");
+            string[] mailList = sourceText.Split("&"); // поскольку по заданию нужны только адреса эл. почты
 
             for (int i = 0; i < mailList.Length; i++)
             {
                 SearchMail(ref mailList[i]);
+
+                if (mailList[i].Length == 0) continue;
+
                 targetText += mailList[i] + "\n";
             }
 
-            File.WriteAllText(target + @"\mailList.txt", targetText);
+            File.WriteAllText(target + "mailList.txt", targetText);
         }
 
         public static void SearchMail(ref string s)
@@ -33,7 +36,7 @@ namespace Task3
                 if (arr[i].Contains("@"))
                 {
                     s = arr[i];
-                    break;
+                    return;
                 }
             }
             s = string.Empty;
